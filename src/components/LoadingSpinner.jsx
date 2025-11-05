e26ef21d3a19a5553edc0d9df36b9659df298f28
dotandru76 <dotan.druckman@gmail.com>
2025-11-05 08:36:05 +0200

    5

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,10 @@
+// src/components/LoadingSpinner.jsx
+import React from 'react';
+
+const LoadingSpinner = () => (
+    <div className="text-center p-5">
+        <div className="spinner w-8 h-8 mx-auto border-t-primary-blue border-r-primary-blue"></div>
+    </div>
+);
+
+export default LoadingSpinner;
